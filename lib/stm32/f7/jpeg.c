561b14fadd74d0cb2b01c2ea6972cff478c7eaa2
Oliver Meier <h2obrain@gmail.com>
2019-05-22 12:48:00 +0200

    stm32f7: added jpeg codec

---
+++ lib/stm32/f7/jpeg.c
@@ -0,0 +1,30 @@
+/**
+ * @defgroup jpeg_file JPEG
+ * @ingroup STM32F7xx
+ * @brief <b>Defined Constants and Types for the STM32F7xx JPEG codec</b>
+ * @version 1.0.0
+ * @date 22 May 2019
+ *
+ * This library supports the JPEG codec in the STM32F7xx series of ARM Cortex
+ * Microcontrollers by ST Microelectronics.
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/jpeg.h>
